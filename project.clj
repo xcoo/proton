18513b38e09e854f64df5d47345d81a43a70bfa9
Takashi AOKI <me@tak.sh>
2018-03-07 13:06:19 +0900

    set version to 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject proton "0.1.6"
+(defproject proton "0.1.7-SNAPSHOT"
   :description "Utilities library for Clojure/Script"
   :url "https://github.com/xcoo/proton"
   :license {:name "Apache License, Version 2.0"
