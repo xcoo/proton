bc8c7553e9fed6ee0aa3bcb348d21ad31999ba05
Toshiki Takeuchi <take@xcoo.jp>
2018-03-12 18:58:39 +0900

    Fix exit status of cljs test

--- test/proton/test.cljs
+++ test/proton/test.cljs
@@ -1,5 +1,6 @@
 (ns proton.test
   (:require [cljs.test]
+            [goog.object :as gobj]
             [proton.core-test]
             [proton.pattern-test]
             [proton.string-test]
@@ -7,6 +8,15 @@
             [proton.uri-test]))
 
 (enable-console-print!)
+
+(defn- exit
+  [status]
+  (if-let [nodejs-exit (and (exists? js/process) (gobj/get js/process "exit"))]
+    (nodejs-exit status)
+    (println "Exit function not found")))
+
+(defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
+  (exit (if (cljs.test/successful? m) 0 1)))
 
 (defn -main
   [& args]
