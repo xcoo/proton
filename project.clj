e1562a0ea57090cab245a55298ef0556d492e954
Takashi AOKI <me@tak.sh>
2021-07-13 18:45:52 +0900

    chore: release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject proton "0.2.0-SNAPSHOT"
+(defproject proton "0.2.0"
   :description "Utilities for Clojure/Script"
   :url "https://github.com/xcoo/proton"
   :license {:name "Apache License, Version 2.0"
