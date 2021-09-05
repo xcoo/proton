cc2689eb9ded92c33c9ed88d69a05e01591e8fe1
Takashi AOKI <me@tak.sh>
2021-09-05 18:41:15 +0900

    chore: prepare next development iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject proton "0.2.2"
+(defproject proton "0.2.3-SNAPSHOT"
   :description "Utilities for Clojure/Script"
   :url "https://github.com/xcoo/proton"
   :license {:name "Apache License, Version 2.0"
